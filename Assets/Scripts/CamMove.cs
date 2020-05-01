23dd8dd7c26a2ff50c909e472b988ad70a9c8917
rfoldbirk <rasmusfoldberg@gmail.com>
2020-05-01 13:13:11 +0200

    Godot :)

---
+++ Assets/Scripts/CamMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float speed = 5;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.mousePosition.x >= Screen.width - 100)
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.mousePosition.x <= 100)
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+    }
+
+}
---
+++ Assets/controlCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class controlCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hej");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
