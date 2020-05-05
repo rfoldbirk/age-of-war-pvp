a91f56d275bb5f9275089fb83d085d008992f07c
Rasmus Foldberg <rasmusfoldberg@gmail.com>
2020-05-05 21:08:21 +0200

    De sidste filer

--- Scripts/Menu/menu_pos.gd
+++ Scripts/Menu/menu_pos.gd
@@ -1,20 +1,31 @@
 extends Control
 
+onready var GAME = get_node("/root/Game")
+onready var MENU = GAME.get_node("/Control/Menu")
+
 func _process(_delta):
 	var camPos = get_parent().get_node("Camera2D").get_camera_screen_center()
 	set_position(Vector2(camPos.x-1200, camPos.y-900))
 	pass
 
 
+
 func _on_TextureButton_mouse_entered(ch):
-	var price = get_node("/root/Game").get("Characters")[ch]["price"]
+	var price = GAME.get("Characters")[ch]["price"]
 	get_node("./Price").text = str(ch, ": ", price, "$")
 
 func _on_TextureButton_pressed(ch):
-	if get_node("/root/Game").get("state") == "game":
-		var dir = get_node("/root/Game").get("DIR")
-		get_node("/root/Game").requestCharacter(ch, dir)
+	if GAME.get("state") == "game":
+		var dir = GAME.get("DIR")
+		GAME.requestCharacter(ch, dir)
+
+
+func _on_menu_btn_pressed(ch):
+	if ch == "local":
+		GAME.go_local()
+
+	GAME.establish_connection()
 
 
 func _on_Button_pressed():
-	OS.execute(OS.get_executable_path(), OS.get_cmdline_args(), false, [])
+	var _process = OS.execute(OS.get_executable_path(), OS.get_cmdline_args(), false, [])
--- Scripts/ws.gd
+++ Scripts/ws.gd
@@ -1,22 +1,35 @@
 extends Node2D
 
+var address = "nfs.codes:2052"
+func go_local():
+	address = "localhost:3000"
+
 var _client = WebSocketClient.new()
 func _ready():
-	print(JSON.parse('{"jeg":"elsker","mande":{"bollor":"tis"}}').result)
+	# print(JSON.parse('{"jeg":"elsker","mande":{"bollor":"tis"}}').result)
+	# establish_connection()
+	pass
+
+
+func establish_connection():
 	_client.connect("connection_closed", self, "_closed")
 	_client.connect("connection_error", self, "_closed")
 	_client.connect("connection_established", self, "_connected")
 	_client.connect("data_received", self, "_on_data")
 
-	var err = _client.connect_to_url("ws://nfs.codes:2052") # nfs.codes:2052
+	var err = _client.connect_to_url(str("ws://", address)) # nfs.codes:2052
 	if err != OK:
 		print("Unable to connect")
+	else:
+		var menu = get_node_or_null("/root/Game/Control/Menu")
+		if menu != null:
+			menu.queue_free()
 
 func _closed(was_clean = false):
 	print("Closed, clean: ", was_clean)
 	set_process(false)
 
-func _connected(proto = ""):
+func _connected(_proto = ""):
 	_client.get_peer(1).put_packet('{"Test":"Packet"}'.to_utf8())
 
 func _on_data():
